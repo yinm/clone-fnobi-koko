51f70aadc9a62b33c77479e058a5bfa1a9491b4d
yinm <yinm.cminor9@gmail.com>
2018-04-15 13:22:11 +0900

    Add startServer()

--- index.js
+++ index.js
@@ -50,3 +50,78 @@ Koko.prototype.start = function() {
     this.open()
   }.bind(this))
 }
+
+Koko.prototype.startServer = function(callback) {
+  const proxyURL = this.proxyURL
+  let proxy
+
+  const app = express()
+
+  if (proxyURL) {
+    proxy = new Proxy(proxyURL)
+    console.log('proxy\t: %s:%d'.info, proxy.host, proxy.port)
+  }
+
+  console.log('php\t: %s'.info, this.usePHP ? 'on' : 'off')
+  console.log('md\t: %s'.info, this.useMarkdown ? 'on' : 'off')
+
+  app.configure(function() {
+    app.use(express.bodyParser())
+
+    if (this.usePHP) {
+      app.set('views', this.rot)
+      app.engine('php', phpExpress.engine)
+      app.set('view engine', 'php')
+
+      app.use(app.router)
+    }
+
+    if (this.useMarkdown) {
+      app.use(function(req, res, next) {
+        if (! /\.md$/.test(req.url)) {
+          next()
+          return
+        }
+        this.renderMarkdown(req, res)
+      }.bind(this))
+    }
+
+    if (this.htmlHandler) {
+      app.use(function(req, res, next) {
+        if (! /\.html$/.test(req.url)) {
+          next()
+          return
+        }
+        this.handleHtmlWithCustomHandler(req, res)
+      }.bind(this))
+    }
+
+    app.use(express.static(this.root))
+    app.use(express.directory(this.root))
+
+    app.use(function(req, res, next) {
+      if (!proxy) {
+        return next()
+      }
+      proxy.proxyRequest(req, res)
+    })
+  }.bind(this))
+
+  if (this.usePHP) {
+    app.all(/.+\.php$/, phpExpress.router)
+  }
+
+  async.waterfall([
+    emptyPort.bind(this, {}),
+    function(p, next) {
+      this.port = this.staticPort || p
+
+      http.createServer(app).listen(this.port, next)
+    }.bind(this),
+    function(next) {
+      console.log('[listen %d]'.info, this.port)
+
+      next()
+    }.bind(this)
+  ], callback)
+}
