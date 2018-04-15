3660ee93872d0225452cfa6d922cf29792d42a16
yinm <yinm.cminor9@gmail.com>
2018-04-15 11:28:41 +0900

    Add class

---
+++ index.js
@@ -0,0 +1,37 @@
+const
+  fs = require('fs'),
+  path = require('path'),
+  util = require('util'),
+  async = require('async'),
+  http = require('http'),
+  express = require('express'),
+  emptyPort = require('empty-port'),
+  colors = require('colors'),
+  opn = require('opn'),
+
+  Proxy = require('./lib/Proxy'),
+  localIP = require('./lib/localIP'),
+
+  phpExpress = require('php-express')(),
+  marked = require('marked')
+
+const Koko = function(root, opt) {
+  colors.setTheme({
+    info: 'green',
+    warn: 'yellow',
+    error: 'red'
+  })
+
+  if (!fs.existsSync(root)) {
+    console.error('%s doesn\'t exist.'.error, root)
+  }
+  console.log('document root\t: %s'.info, root)
+
+  this.root = root
+  this.openPath = opt.openPath
+  this.staticPort = opt.staticPort
+  this.proxyURL = opt.proxyURL
+  this.usePHP = opt.usePHP
+  this.useMarkdown = opt.useMarkdown
+  this.htmlHandler = opt.htmlHandler
+}
